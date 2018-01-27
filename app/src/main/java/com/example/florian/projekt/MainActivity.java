9ddfd139a7b4ac7808e6e576fd7c703f7aff72cc
Florian Zimmer <florian.zimmer@uni-jena.de>
2018-01-27 10:03:48 +0100

    Action Button geht

--- app/src/main/java/com/example/florian/projekt/MainActivity.java
+++ app/src/main/java/com/example/florian/projekt/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.florian.projekt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.*;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       final Button def = (Button) findViewById(R.id.button3);
+       final Button qui = (Button) findViewById(R.id.button2);
+
+       def.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+
+                   System.exit(0);
+               }
+
+       });
+
+        qui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int ce = v.getId();
+
+                if(ce == R.id.button2){
+
+                    Intent intent = new Intent(MainActivity.this, Quiz.class);
+                    startActivity(intent);
+                }
+            }
+
+        });
     }
 }
